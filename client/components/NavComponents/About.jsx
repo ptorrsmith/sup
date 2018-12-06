df6f4b45085e91a1775c84b289cc4ad2d60595f4
Rubymoyes <moyesruby@gmail.com>
2018-12-07 11:34:57 +1300

    webpack conflicts need resolving

--- client/components/NavComponents/About.jsx
+++ client/components/NavComponents/About.jsx
@@ -7,13 +7,38 @@ import { connect } from 'react-redux'
 const About = () => (
     <div>
 
-        <div className="about_body">
-        <p>Hello from About component body.</p>
+        <div className="about_header">
+            <h2>About Sup:</h2>
         </div>
 
-    {/* Information about what the website provides */}
+        <div className="about_body">
+            <p>
+                <em>Sup</em> is a progressive web application specifically designed to provide support for New Zealanders in need.
+        
+        Sup gives users the opportunity to open and view service providers in Wellington, including their locations,
+        facilities, opening and closing hours, and the availability of the service. Services include emergency shelters, food banks,
+        bathroom facilities and soup kitchens.
+        
+        Sup also allows registered providers to update their service details from the admin panel, and gives the users real-time updates
+        directly from the service, notifying them about recent changes.
+        
+        Sup is an independent site, currently being updated and maintained by students at Enspiral Dev Academy.
+        
+        Meet The <em>Sup</em> Team:
+        <ul>
+            <li>Bobbi Kerei - Founder, Developer</li>
+            <li>Brandon Ross - Developer</li>
+            <li>James Turner - Developer</li>
+            <li>Peter Smith - Developer</li>
+            <li>Ruby Moyes - Developer</li>
+        </ul>
+        </p>
+        
+        </div>
 
-</div>
+        {/* Information about what the website provides */}
+
+    </div>
 
 )
 
