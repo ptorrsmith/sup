8cd157c3647f228c882b3e1658e14f5c964f8a18
Rubymoyes <moyesruby@gmail.com>
2018-12-06 14:25:31 +1300

    added more necessary files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './client/index.js',
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          // exclude: /node_modules/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map'
+  }
