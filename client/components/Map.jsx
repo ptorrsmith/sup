85dab8113de98e266b47010ea910861061e5ba08
Rubymoyes <moyesruby@gmail.com>
2018-12-06 17:19:24 +1300

    changes to Map

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -2,6 +2,8 @@
 // CHILD OF APP
 
 import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import {connect} from 'react-redux'
 
 import ViewPreview from './ViewPreview'
 import Markers from './Markers'
