ef8aed72d0466db7007a9de3102095b9916b1ac8
bkerei <bwkerei@gmail.com>
2018-12-06 15:51:46 +1300

    removing index

--- client/index.js
+++ client/index.js
@@ -6,8 +6,8 @@ import {Provider} from 'react-redux'
 import { createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
-import reducers from './reducers'
-import App from './components/App'
+// import reducers from './reducers'
+// import App from './components/App'
 
 let store = createStore(reducers, compose(
     applyMiddleware(thunkMiddleware),
