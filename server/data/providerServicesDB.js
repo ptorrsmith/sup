179a4243632a92acf28635da176104b17b1c0198
ptorrsmith <peter@ptorrsmith.com>
2018-12-07 00:14:23 +1300

    tidy up

--- server/data/providerServicesDB.js
+++ server/data/providerServicesDB.js
@@ -10,19 +10,12 @@ module.exports = {
   
   
   function getProviderServices(geoBoxSearch, ignoreProvidersArray, db = connection) {     
-      // const dataPromise = db('providers')
-      //     .then(providers => {
-      //         const joinServicesPromise = db('services')
-      //     })
-      
-      
-      
-        const dataPromise = db('providers AS p')
-          .leftOuterJoin('services AS s', 's.provider_id', 'p.id'  )
-          .leftOuterJoin('service_types AS st', 's.service_type_id', 'st.id')
-          // .select('p.*', 's.*', 'st.*', 'p.id AS provicer_id', 's.id AS service_id') //.where('long', p.long)
-          .select(
-            'p.id as provider_id',
+      const dataPromise = db('providers AS p')
+      .leftOuterJoin('services AS s', 's.provider_id', 'p.id'  )
+      .leftOuterJoin('service_types AS st', 's.service_type_id', 'st.id')
+      // .select('p.*', 's.*', 'st.*', 'p.id AS provicer_id', 's.id AS service_id') //.where('long', p.long)
+      .select(
+          'p.id as provider_id',
             'p.name as provider_name',
             'p.description as provider_description',
             'p.lat',
@@ -42,35 +35,22 @@ module.exports = {
             'st.name as service_type_name',
             'st.code as service_type_code',
             'st.icon'
-          )
-    //   .select(
-    //     'u.id',
-    //     'u.first_name',
-    //     'u.last_name',
-    //     'u.username',
-    //     'u.image_url',
-    //     'u.is_admin',
-    //     'u.phone',
-    //     'u.info',
-    //     'la.email',
-    //     'cu.customer_id',
-    //     'cu.department_id'
-    // )
-    // .from('user AS u')
-    // .leftJoin('local_auth AS la', 'la.user_id', 'u.id')
-    // .leftJoin('customer_user AS cu', 'cu.user_id', 'u.id')
-    // .where('u.id', '=', id)
-    // .first()  
-
-
-
-    //   console.log("providerServicesDB getprovsvs dataPromise : ", dataPromise)
-      return dataPromise
-    }
-    
-  // returning promises to mock the knex asynchronous behaviour
-  // this was mocking the database
-  // return new Promise((resolve, reject) => {
+            )
+            
+            // thinking of nesting db calls to make proper shape response object
+            // const dataPromise = db('providers')
+            //     .then(providers => {
+            //         const joinServicesPromise = db('services')
+            //     })
+            
+            //   console.log("providerServicesDB getprovsvs dataPromise : ", dataPromise)
+            
+            return dataPromise
+        }
+        
+        // returning promises to mock the knex asynchronous behaviour
+        // this was mocking the database
+        // return new Promise((resolve, reject) => {
   //   resolve(providerServices.filter(providerService => !owner_id || providerService.owner_id == owner_id))
   // })
   // }
