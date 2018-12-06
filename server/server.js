7dd69d0fba50af82adaff1e9e8cd455965816b3a
Rubymoyes <moyesruby@gmail.com>
2018-12-06 14:51:47 +1300

    more changes made, to merge into dev

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ var server = express()
 
 server.use(cors('*'))
 
-server.use(passport.initialize())
+// server.use(passport.initialize())
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/auth', require('./routes/auth'))
+// server.use('/api/auth', require('./routes/auth'))
 // server.use('/api/meetings', require('./routes/meetings'))
 // server.use('/api/users', require('./routes/users'))
 
