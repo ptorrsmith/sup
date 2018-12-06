8858e01b459ecd701646da7046fb3d15db752941
Rubymoyes <moyesruby@gmail.com>
2018-12-07 11:36:16 +1300

    added some tags to About, but conflicts in webpack still need fixing to work further

--- client/components/NavComponents/About.jsx
+++ client/components/NavComponents/About.jsx
@@ -14,26 +14,26 @@ const About = () => (
         <div className="about_body">
             <p>
                 <em>Sup</em> is a progressive web application specifically designed to provide support for New Zealanders in need.
-        
+
         Sup gives users the opportunity to open and view service providers in Wellington, including their locations,
         facilities, opening and closing hours, and the availability of the service. Services include emergency shelters, food banks,
-        bathroom facilities and soup kitchens.
-        
+        bathroom facilities and soup kitchens.<br></br>
+
         Sup also allows registered providers to update their service details from the admin panel, and gives the users real-time updates
-        directly from the service, notifying them about recent changes.
-        
-        Sup is an independent site, currently being updated and maintained by students at Enspiral Dev Academy.
-        
+        directly from the service, notifying them about recent changes.<br></br>
+
+        Sup is an independent site, currently being updated and maintained by students at Enspiral Dev Academy.<br></br>
+
         Meet The <em>Sup</em> Team:
         <ul>
-            <li>Bobbi Kerei - Founder, Developer</li>
-            <li>Brandon Ross - Developer</li>
-            <li>James Turner - Developer</li>
-            <li>Peter Smith - Developer</li>
-            <li>Ruby Moyes - Developer</li>
-        </ul>
-        </p>
-        
+                    <li>Bobbi Kerei - Founder, Developer</li>
+                    <li>Brandon Ross - Developer</li>
+                    <li>James Turner - Developer</li>
+                    <li>Peter Smith - Developer</li>
+                    <li>Ruby Moyes - Developer</li>
+                </ul>
+            </p>
+
         </div>
 
         {/* Information about what the website provides */}
