07ab1d0501281ef2105b13bb7613e0648921ce2f
James <james-is-a-name@users.noreply.github.com>
2018-12-07 00:06:32 +1300

    add reducer thunk example

--- client/reducers/providers.js
+++ client/reducers/providers.js
@@ -1,9 +1,30 @@
 
+import {getData} from '../utils/tempData'
+
 
 const initialState = {
   providers: [],
   isFetching: false
 }
+
+
+export const fetchData = () => {
+  return dispatch => {
+    dispatch({type : 'GETTING_PROVIDERS'})
+
+    getData().then( (data) => {
+
+      addData(data)
+
+      dispatch({
+        type: 'RECEIVED_PROVIDERS',
+        providers: data
+      })
+    })
+  }
+}
+
+
 
 export default function auth(state = initialState, action) {
   switch (action.type) {
