0fce69309e62de0e77d189442afe611f76568cd3
Garry <garry@garry.bar>
2018-12-07 13:40:18 +1300

    fixed services, providers, and providerServices routes

--- server/data/providersDB.js
+++ server/data/providersDB.js
@@ -2,33 +2,32 @@
 
 module.exports = {
     getProviders
-  }
-  
-  const config = require('../../knexfile').development // [environment]
-  const connection = require('knex')(config)
-  const _ = require('lodash')
-  
-  
-  function getProviders(geoBoxSearch, ignoreProvidersArray, db = connection) {     
-      console.log("ProvidersDB, getProvicers")
-      const dataPromise = db('providers AS p')
-      // .select('p.*', 's.*', 'st.*', 'p.id AS provicer_id', 's.id AS service_id') //.where('long', p.long)
-      .select(
-          'p.id as provider_id',
-            'p.name as provider_name',
-            'p.description as provider_description',
+}
+
+const config = require('../../knexfile').development // [environment]
+const connection = require('knex')(config)
+const _ = require('lodash')
+
+
+function getProviders(geoBoxSearch, ignoreProvidersArray, db = connection) {
+    console.log("ProvidersDB, getProvicers")
+    const dataPromise = db('providers AS p')
+        // .select('p.*', 's.*', 'st.*', 'p.id AS provicer_id', 's.id AS service_id') //.where('long', p.long)
+        .select(
+            'p.id',
+            'p.name',
+            'p.description',
             'p.lat',
             'p.long',
             'p.hours',
-            'p.update_message', 
+            'p.update_message',
             'p.address',
             'p.email',
             'p.website_url',
-            'p.updated_at as provider_updated_at'
-            )
-            
-            return dataPromise
-        }
-  
-  
-  
+            'p.updated_at'
+        )
+
+    return dataPromise
+}
+
+
--- server/data/servicesDB.js
+++ server/data/servicesDB.js
@@ -2,28 +2,30 @@
 
 module.exports = {
     getServicesForProviders
-  }
-  
-  const config = require('../../knexfile').development // [environment]
-  const connection = require('knex')(config)
-  const _ = require('lodash')
-  
-  
-  function getServicesForProviders(providersIdList, serviceSearchObj, db = connection) {     
-      const dataPromise = db('services AS s')
-      .leftOuterJoin('service_types AS st', 's.service_type_id', 'st.id')
-      // .select('p.*', 's.*', 'st.*', 'p.id AS provicer_id', 's.id AS service_id') //.where('long', p.long)
-      .select(
-            's.name as service_name',
-            's.unit as units',
+}
+
+const config = require('../../knexfile').development // [environment]
+const connection = require('knex')(config)
+const _ = require('lodash')
+
+
+function getServicesForProviders(providersIdList, serviceSearchObj, db = connection) {
+    const dataPromise = db('services AS s')
+        .leftOuterJoin('service_types AS st', 's.service_type_id', 'st.id')
+        // .select('p.*', 's.*', 'st.*', 'p.id AS provicer_id', 's.id AS service_id') //.where('long', p.long)
+        .select(
+            's.id',
+            's.provider_id',
+            's.name',
+            's.unit',
             's.qty_default',
             's.qty_remaining',
-            's.updated_at as service_updated_at',
-            'st.id as service_type_id',
-            'st.name as service_type_name',
-            'st.code as service_type_code',
+            's.updated_at',
+            's.service_type_id',
+            'st.name',
+            'st.code',
             'st.icon'
-            )
-            .whereIn('s.provider_id', providersIdList)                       
-            return dataPromise
-        }
+        )
+    // .whereIn('s.provider_id', providersIdList)
+    return dataPromise
+}
