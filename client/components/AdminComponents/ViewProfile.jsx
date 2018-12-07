5784a88a1d7d23ec34475aecd9ac414e555183e5
Rubymoyes <moyesruby@gmail.com>
2018-12-07 14:37:23 +1300

    added stuff to components. might need some guidance at some point on how to get stuff from the database show up on the components

--- client/components/AdminComponents/ViewProfile.jsx
+++ client/components/AdminComponents/ViewProfile.jsx
@@ -1,11 +1,10 @@
-// CHILD OF VIEW PREVIEW
+// CHILD OF VIEW PREVIEW - REDIRECTS HERE FROM PROFILE LIST
 import React from 'react'
 
 const ViewProfile = () => (
     <React.Fragment>
 
         {/* The full detailed page of a provider. */}
-
 
     </React.Fragment>
 )
