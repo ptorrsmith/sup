b7170b4dfa92ff3e4ff191ff06610179bf41cf1d
Rubymoyes <moyesruby@gmail.com>
2018-12-08 15:10:16 +1300

    something wierd happened with the edit page and I dont quite know how I will fix that

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,7 +6,7 @@ import Map from './Map'
 import Admin from './Admin'
 import Nav from './Nav'
 import Sidebar from './TDC/Sidebar'
-import ViewProfile from './AdminComponents/ViewProfile'
+import AdminProfile from './AdminComponents/AdminProfile'
 import AddProfile from './AdminComponents/AddProfile'
 import EditProfile from './AdminComponents/EditProfile'
 
@@ -30,9 +30,10 @@ const App = () => (
             <Route exact path="/" component={Map} />
             <Route exact path="/" component={Nav} />
             <Route exact path="/admin" component={Admin} />
-            <Route exact path="/admin/view/:id" component={ViewProfile} />
+            {/* Admin Profile has the ability to edit the profile, depending on the auth of the admin user */}
+            <Route exact path="/admin/:id" component={AdminProfile} />
             <Route exact path="/admin/add" component={AddProfile} />
-            <Route exact path="/admin/edit" component={EditProfile} />
+            <Route exact path="/admin/:id/edit" component={EditProfile} />
 
         </div>
     </Router >
