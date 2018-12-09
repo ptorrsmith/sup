0620c2404cb18a106abd89407d5cab27244253ad
Rubymoyes <moyesruby@gmail.com>
2018-12-10 11:24:48 +1300

    building the component LiveUpdate

--- client/components/AdminComponents/LiveUpdate.jsx
+++ client/components/AdminComponents/LiveUpdate.jsx
@@ -1,29 +1,47 @@
 import React from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-import { currentProvider } from '../../actions/index'
+import { currentProvider } from '../../reducers/currentProvider'
+import { setQtyRemaining, setUpdate, setStatus } from '../../actions'
 
 class LiveUpdate extends React.Component {
     constructor(props) {
         super(props)
+        // set initial state
+        // no local state?
+
+        // bind functions
     }
 
-    render () {
+    // componentWillMount() {
+    //     console.log("Will mount")
+
+    //     this.props._____
+    // }
+
+    // componentDidMount() {
+    //     console.log("mounted")
+    // }
+
+    render() {
+        // const providersAndServices = this.props.providers.providers
+        // console.log("ProfileList render providersAndServices >>>> ", providersAndServices)
+
         return <div>Hello from LiveUpdate Stateful</div>
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-      state
+        state
     }
-  }
-  
-  const mapDispatchToProps = (dispatch) => {
+}
+
+const mapDispatchToProps = (dispatch) => {
     return {
-      dispatch
+        dispatch
     }
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(LiveUpdate)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LiveUpdate)
