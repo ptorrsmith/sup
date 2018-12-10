5ad2a0ef5a71fd8ccf6888da7c12502548ec08f2
Arry <Arry@Arry.arr>
2018-12-10 18:14:14 +1300

    sets current provider on refresh

--- client/components/TDC/Profile.jsx
+++ client/components/TDC/Profile.jsx
@@ -4,31 +4,16 @@ import { connect } from "react-redux";
 import Divider from "@material-ui/core/Divider";
 // import { theBackground, grid_container, profile_header, profole_body } from '../../../public/style'
 
-import { setCurrentProvider } from "../../actions";
-
-function setCurrentProfile(dispatch) {
-  dispatch(
-    setCurrentProvider({
-      id: 1,
-      name: "Website Test Data - The Mens Night Shelter",
-      description:
-        "Website Test Data - We provide 3 levels of accommodation support:<br> <br>stage 1: dormitory style accommodation which including shower and laundry facilities ($10 a night)<br>stage 2: hostel room accommodation ($80 a week)<br>stage 3: community support (tempoary emergency shelter)<br><br>Dormitory services include a bed for the night, shower facilities, clothes washed, cup of tea. Occasionally some light food is donated and made available.<br><br>Facilities: Bathroom, Laundry & Food.",
-      address: "304 Taranki St, Mt. Cook, Wellington 6011",
-      phone: "(04) 385-9546",
-      update_message: "Website Test Data - functional",
-      lat: -41.300598,
-      long: 174.774082,
-      email: "menshelter@hotmail.com",
-      website_url: "http://wellingtonnightshelter.org.nz/",
-      hours:
-        "Open: 7 days a week, all year round<br>Checkin: 5:30pm – 9:00pm<br>Checkout: 6:00am – 7:30am"
-    })
-  );
-}
+import { setCurrentProvider, fetchProvider } from "../../actions";
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchProvider(id);
   }
 
   render() {
@@ -111,7 +96,18 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(Profile);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProvider: params => {
+      return dispatch(fetchProvider(params));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Profile);
 
 // return (
 
