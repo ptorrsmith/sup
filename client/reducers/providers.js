22ce79007f8303ac338d514606ed14597a6358df
ptorrsmith <peter@ptorrsmith.com>
2018-12-10 16:03:16 +1300

    Set new or updated provider into redux state of providers

--- client/reducers/providers.js
+++ client/reducers/providers.js
@@ -22,10 +22,28 @@ export default function auth(state = initialState, action) {
     //   provider: action.provider
     // }
     case 'RECEIVED_PROVIDERS':
+      console.log("reducers > providers > RECEIVED_PROVIDERS (action) ", action)
       return {
         ...state,
         isFetching: false,
         providers: action.providers
+      }
+    case 'RECEIVED_PROVIDER':
+      // when receive an individual provider, the currentProviders reducer will set that to be the currentProvider redux state
+      // however we also need to update the provider in our "providers" redux state
+      const copyOfProviders = state.providers.slice()
+      const newOrupdatedProvider = action.currentProvider
+      const index = copyOfProviders.findIndex(provider => provider.id == newOrupdatedProvider.id)
+      // could be a number 0 or more, if already exists, or could be negative number if not exists (so we must add it)
+      if (index > 0) {
+        copyOfProviders.splice(index, 1, newOrupdatedProvider)
+      } else {
+        copyOfProviders.push(newOrupdatedProvider)
+      }
+      console.log("reducers > providers > RECEIVED_PROVIDER : newOrUpdatedProvider ::::", newOrupdatedProvider, "\nProvidersCopy>>> ", copyOfProviders)
+      return {
+        ...state, // this returns ALL the providers
+        currentProvider: newOrupdatedProvider
       }
     case 'FETCH_PROVIDERS_ERROR':
       return {
