5dcfc85e1df909269d24f61cc586eccc35267f94
Rubymoyes <moyesruby@gmail.com>
2018-12-12 09:22:43 +1300

    deleted unused components to avoid confusion

--- client/components/TDC/LogIn.jsx
+++ client/components/TDC/LogIn.jsx
@@ -7,7 +7,7 @@ import { connect } from 'react-redux'
 const LogIn = () => (
     <React.Fragment>
 
-{/* Form with log in credentials, auth, and everything else
+        {/* Form with log in credentials, auth, and everything else
 needed to allow someone to log in to use the admin page
 
 Will redirect to the admin page once logged in */}
