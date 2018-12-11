1c89d2078fc7162cb3b80d7b332751585b70edef
bkerei <bwkerei@gmail.com>
2018-12-11 23:57:09 +1300

    updated teh fonts on teh sidepanel and incoperated the dividers

--- client/components/NavComponents/HowToUse.jsx
+++ client/components/NavComponents/HowToUse.jsx
@@ -3,17 +3,18 @@
 import React from "react";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Typography from '@material-ui/core/Typography';
+import Divider from "@material-ui/core/Divider";
 
 const HowToUse = () => (
 
     <div>
 
-        <div className="how_to_use_header">
-            <h2>How to Use <em>Sup</em>:</h2>
-        </div>
-
-        <div className="how_to_use_body">
-            <p>
+        <Typography variant="h6" gutterBottom>
+            Getting Satrted on <em>Sup</em>:
+        </Typography>
+        <Divider />
+        <Typography variant="body1" gutterBottom>
               Welcome to <em>SUP</em> Lets get started!
             <ul>
                     <li>Tap on the Map to find a bed, a meal, a food parcel or other service.</li>
@@ -21,11 +22,8 @@ const HowToUse = () => (
                     <li>Use the box on the bottom left to filter for the services you're looking for.</li>
                     <li>If you want the address just zoom in on the map marker or tap the marker for more info.</li>
                 </ul>
-                
-        </p>
-        </div>
+                </Typography>
 
-        {/* Information about what the website provides */}
 
     </div>
 )
